f081b97f3fced7a6132bf388da38eb9d94a2fb4d
tudorcotet <61837741+tudorcotet@users.noreply.github.com>
2020-08-03 15:48:26 +0300

    Testing github commands

--- Analysis.py
+++ Analysis.py
@@ -4,15 +4,14 @@ import numpy as np
 expression_df = pd.read_csv("./data/ExpressionGenes.txt", header = 0, sep = "\t")
 expression_df
 
-
-muscle_genes = pd.read_csv("./data/Muscle differentiation genes.csv", header = 0, sep = ",")
-muscle_genes = muscle_genes["Gene symbol"].tolist()
+muscle_genes = (pd.read_csv("./data/Muscle differentiation genes - Genes.csv", header = 0, sep = ",")
+                  ["Gene symbol"].tolist()
+               )
 muscle_genes = [gene.upper() for gene in muscle_genes]
 muscle_genes
 
-
-muscle_expression = expression_df.filter(like = "Muscle")
-muscle_expression = muscle_expression.assign(Gene = expression_df["ApprovedSymbol"])
+muscle_expression = (expression_df.filter(like = "Muscle")
+                                  .assign(Gene = expression_df["ApprovedSymbol"])
+                    )
 muscle_expression = muscle_expression[muscle_expression["Gene"].isin(muscle_genes)]
-
 muscle_expression
